69a264eedb2c0b0fd671088b8dd15fe717d5aa04
Şahin Üregil <uregilsahin@gmail.com>
2026-05-24 00:33:52 +0300

    feat: Add rest of the gates

--- src/gates/gates.c
+++ src/gates/gates.c
@@ -15,3 +15,30 @@ bit_t and_gate(bit_t a, bit_t b)
     return not_gate(nand_gate(a, b));
 }
 
+bit_t or_gate(bit_t a, bit_t b)
+{
+    return not_gate(
+        and_gate(
+            not_gate(a),
+            not_gate(b)
+        )
+    );
+}
+
+bit_t xor_gate(bit_t a, bit_t b)
+{
+    return or_gate(
+        and_gate(a, not_gate(b)),
+        and_gate(not_gate(a), b)
+    );
+}
+
+bit_t nor_gate(bit_t a, bit_t b)
+{
+    return not_gate(or_gate(a, b));
+}
+
+bit_t xnor_gate(bit_t a, bit_t b)
+{
+    return not_gate(xor_gate(a, b));
+}
