0091fd3e01bdb10defe657b7e65045ec588da266
Şahin Üregil <uregilsahin@gmail.com>
2026-05-24 00:27:32 +0300

    feat: Add not and and gate functions in gates.c

--- src/gates/gates.c
+++ src/gates/gates.c
@@ -4,3 +4,14 @@ bit_t nand_gate(bit_t a, bit_t b)
 {
     return !(a && b);
 }
+
+bit_t not_gate(bit_t a)
+{
+    return nand_gate(a, a);
+}
+
+bit_t and_gate(bit_t a, bit_t b)
+{
+    return not_gate(nand_gate(a, b));
+}
+
