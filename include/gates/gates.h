6e9a1d12202ebb4be952a2e1f8e1a004a09957aa
Şahin Üregil <uregilsahin@gmail.com>
2026-05-24 14:19:33 +0300

    refactor: Rename parameters in gate functions for consistency

--- include/gates/gates.h
+++ include/gates/gates.h
@@ -3,15 +3,15 @@
 
 #include "bit.h"
 
-bit_t nand_gate(bit_t a, bit_t b);
+bit_t nand_gate(bit_t i0, bit_t i1);
 
-bit_t not_gate(bit_t a);
-bit_t and_gate(bit_t a, bit_t b);
-bit_t or_gate(bit_t a, bit_t b);
+bit_t not_gate(bit_t i0);
+bit_t and_gate(bit_t i0, bit_t i1);
+bit_t or_gate(bit_t i0, bit_t i1);
 
-bit_t xor_gate(bit_t a, bit_t b);
+bit_t xor_gate(bit_t i0, bit_t i1);
 
-bit_t nor_gate(bit_t a, bit_t b);
-bit_t xnor_gate(bit_t a, bit_t b);
+bit_t nor_gate(bit_t i0, bit_t i1);
+bit_t xnor_gate(bit_t i0, bit_t i1);
 
 #endif // GATES_H
