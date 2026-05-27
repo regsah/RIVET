a2f2439ae3fb03ac907979a4472a3e0d0b048dcb
Şahin Üregil <uregilsahin@gmail.com>
2026-05-27 13:34:03 +0300

    feat: Add test for 8-to-1 multiplexer in test_mux.c

--- tests/combinational/test_mux.c
+++ tests/combinational/test_mux.c
@@ -37,11 +37,41 @@ int test_mux4_1()
     return fail_counter;
 }
 
+int test_mux8_1()
+{
+    int fail_counter = 0;
+    for (int I = 0; I < 256; I++) {
+        for (int S = 0; S < 8; S++) {
+            bit_t expected = (I >> S) & 1;
+            byte_t input = { .bits = {
+                (I >> 0) & 1,
+                (I >> 1) & 1,
+                (I >> 2) & 1,
+                (I >> 3) & 1,
+                (I >> 4) & 1,
+                (I >> 5) & 1,
+                (I >> 6) & 1,
+                (I >> 7) & 1
+            }};
+            bit_t actual = mux8_1(
+                input,
+                (S >> 0) & 1,
+                (S >> 1) & 1,
+                (S >> 2) & 1
+            );
+            ASSERT_EQ_BIT(expected, actual);
+        }
+    }
+    if (fail_counter > 0) printf("test_mux8_1: %d tests failed\n", fail_counter);
+    return fail_counter;
+}
+
 int test_mux()
 {
     int mux_fail_counter = 0;
     mux_fail_counter += test_mux2_1();
     mux_fail_counter += test_mux4_1();
+    mux_fail_counter += test_mux8_1();
     printf("Total mux tests failed: %d\n", mux_fail_counter);
     return mux_fail_counter;
 }
