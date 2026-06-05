cba4fdb18d7e4a64c026ea64e6bf9d2e1125b657
Şahin Üregil <uregilsahin@gmail.com>
2026-06-05 23:48:47 +0300

    feat: Implement adder function in adder.c

--- src/combinational/adder.c
+++ src/combinational/adder.c
@@ -7,3 +7,14 @@ adder_result_t half_adder(bit_t i0, bit_t i1)
         .carry = and_gate(i0, i1)
     };
 }
+
+adder_result_t adder(bit_t i0, bit_t i1, bit_t carry)
+{
+    adder_result_t first_half = half_adder(i0, i1);
+    adder_result_t second_half = half_adder(first_half.sum, carry);
+
+    return (adder_result_t){
+        .sum = second_half.sum,
+        .carry = or_gate(first_half.carry, second_half.carry)
+    };
+}
