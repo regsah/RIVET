a2fdf7d6267fe045b294539c89aee88e2d3b1384
Şahin Üregil <uregilsahin@gmail.com>
2026-06-05 23:58:09 +0300

    refactor: Simplify adder functions for clarity and consistency

--- src/combinational/adder.c
+++ src/combinational/adder.c
@@ -2,10 +2,10 @@
 
 adder_result_t half_adder(bit_t i0, bit_t i1)
 {
-    return (adder_result_t){
-        .sum = xor_gate(i0, i1),
-        .carry = and_gate(i0, i1)
-    };
+    adder_result_t result;
+    result.sum = xor_gate(i0, i1);
+    result.carry = and_gate(i0, i1);
+    return result;
 }
 
 adder_result_t adder(bit_t i0, bit_t i1, bit_t carry)
@@ -13,8 +13,8 @@ adder_result_t adder(bit_t i0, bit_t i1, bit_t carry)
     adder_result_t first_half = half_adder(i0, i1);
     adder_result_t second_half = half_adder(first_half.sum, carry);
 
-    return (adder_result_t){
-        .sum = second_half.sum,
-        .carry = or_gate(first_half.carry, second_half.carry)
-    };
+    adder_result_t result;
+    result.sum = second_half.sum;
+    result.carry = or_gate(first_half.carry, second_half.carry);
+    return result;
 }
