730499e327cd27cf3aa4f303bcb6e9a2616b3a2a
Şahin Üregil <uregilsahin@gmail.com>
2026-07-12 22:52:39 +0300

    feat: implement 32-bit multiplexer and AND gate functionality

--- src/combinational/mux.c
+++ src/combinational/mux.c
@@ -25,3 +25,15 @@ bit_t mux8_1(byte_t I, bit_t s0, bit_t s1, bit_t s2)
         s2
     );
 }
+
+bit_t mux32_1(word_t I, bit_t s0, bit_t s1, bit_t s2, bit_t s3, bit_t s4)
+{
+    return mux4_1(
+        mux8_1((byte_t) {.bits = {I.bits[0], I.bits[1], I.bits[2], I.bits[3], I.bits[4], I.bits[5], I.bits[6], I.bits[7]}}, s0, s1, s2),
+        mux8_1((byte_t) {.bits = {I.bits[8], I.bits[9], I.bits[10], I.bits[11], I.bits[12], I.bits[13], I.bits[14], I.bits[15]}}, s0, s1, s2),
+        mux8_1((byte_t) {.bits = {I.bits[16], I.bits[17], I.bits[18], I.bits[19], I.bits[20], I.bits[21], I.bits[22], I.bits[23]}}, s0, s1, s2),
+        mux8_1((byte_t) {.bits = {I.bits[24], I.bits[25], I.bits[26], I.bits[27], I.bits[28], I.bits[29], I.bits[30], I.bits[31]}}, s0, s1, s2),
+        s3,
+        s4
+    );
+}
--- src/gates/and.c
+++ src/gates/and.c
@@ -19,3 +19,13 @@ bit_t and_gate8(bit_t i0, bit_t i1, bit_t i2, bit_t i3, bit_t i4, bit_t i5, bit_
 {
     return and_gate(and_gate4(i0, i1, i2, i3), and_gate4(i4, i5, i6, i7));
 }
+
+bit_t and_gate32(word_t I)
+{
+    return and_gate4(
+        and_gate8(I.bits[0], I.bits[1], I.bits[2], I.bits[3], I.bits[4], I.bits[5], I.bits[6], I.bits[7]),
+        and_gate8(I.bits[8], I.bits[9], I.bits[10], I.bits[11], I.bits[12], I.bits[13], I.bits[14], I.bits[15]),
+        and_gate8(I.bits[16], I.bits[17], I.bits[18], I.bits[19], I.bits[20], I.bits[21], I.bits[22], I.bits[23]),
+        and_gate8(I.bits[24], I.bits[25], I.bits[26], I.bits[27], I.bits[28], I.bits[29], I.bits[30], I.bits[31])
+    );
+}
